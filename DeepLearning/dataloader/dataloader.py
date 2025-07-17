6a0a6237dcd4fed2f361077b64657df1e69fc179
BenjaminIsaac0111 <benjamintaya0111@gmail.com>
2025-07-17 11:49:49 +0100

    - Start on the finetuning script for active learning pipeline.

--- DeepLearning/dataloader/dataloader.py
+++ DeepLearning/dataloader/dataloader.py
@@ -1,5 +1,7 @@
-from collections import defaultdict
+import json
+import os
 
+import numpy as np
 import tensorflow as tf
 
 try:
@@ -205,82 +207,10 @@ def get_dataset_from_dir(
     return ds
 
 
-def get_balanced_upsampled_dataset(cfg=None, repeat=True, transforms=None, filelists=None, shuffle=True,
-                                   buffer_size=256, batch_size=None):
-    # Convert a single filelist path to a list if necessary
-    if isinstance(filelists, str):
-        filelists = [filelists]  # Wrap the single filelist path in a list
-
-    all_files = []  # List to collect all file paths from the .txt files
-    label_file_dict = defaultdict(list)  # Dictionary to hold files by label
-
-    # Iterate over each .txt file in the list
-    for filelist in filelists:
-        with open(filelist, 'r') as f:
-            # Read the file paths from the .txt file and prepend the directory from attention_unet.config
-            files_in_list = [os.path.join(cfg['DATA_DIR'], line.strip()) for line in f.readlines()]
-            files_in_list = [patch.split('\t')[0] for patch in files_in_list]
-            for file_path in files_in_list:
-                label = get_label_for_file(file_path)
-                label_file_dict[label].append(file_path)
-            all_files.extend(files_in_list)  # Add to the collective list of file paths
-
-    # Find the maximum class count for upsampling
-    max_class_count = max(len(files) for files in label_file_dict.values())
-
-    # Upsample minority classes
-    upsampled_files = []
-    for label, files in label_file_dict.items():
-        upsampled_files.extend(files * (max_class_count // len(files)) + files[:max_class_count % len(files)])
-
-    # Create a dataset from the upsampled list of file paths
-    ds = tf.data.Dataset.from_tensor_slices(upsampled_files)
-
-    if shuffle:
-        # Shuffle the dataset
-        ds = ds.shuffle(buffer_size=buffer_size)
-
-    if repeat:
-        ds = ds.repeat(-1)  # Repeat the dataset indefinitely
-
-    def process_file(filepath):
-        # Process each file path (loading, preprocessing, etc.)
-        image, label = process_path_value_per_class(filepath, cfg['OUT_CHANNELS'])
-        filename = tf.strings.split(filepath, os.sep)[-1]
-        return filename, image, label
-
-    # Apply processing function to each element in the dataset
-    ds = ds.map(process_file, num_parallel_calls=AUTOTUNE)
-
-    # Batch the dataset and randomly sample a subsample in each batch
-    ds = ds.batch(batch_size=batch_size, drop_remainder=True)
-    ds = ds.map(lambda filenames, images, labels: (filenames, images, labels), num_parallel_calls=AUTOTUNE)
-
-    if transforms:
-        # Wrap the transforms to ignore the filename
-        def transform_with_filename(filename, image, label):
-            image, label = transforms(image, label)
-            return filename, image, label
-
-        # Apply additional transformations if provided
-        ds = ds.map(transform_with_filename, num_parallel_calls=AUTOTUNE)
-
-    # Prefetch data for optimized data loading
-    ds = ds.prefetch(AUTOTUNE)
-
-    return ds
-
-
 def get_label_for_file(file_path):
     filename = os.path.basename(file_path)
     label = int(filename[-5])
     return label
-
-
-import json
-import numpy as np
-import tensorflow as tf
-import os
 
 
 # ------------------------------------------------------------------
@@ -319,9 +249,9 @@ def _draw_mask_np(coords, class_ids, height, width,
 def get_dataset_from_json(json_path,
                           images_dir,
                           *,
-                          radius=7,
-                          out_channels=3,
-                          batch_size=8,
+                          radius=8,
+                          out_channels=9,
+                          batch_size=1,
                           shuffle=True,
                           repeat=True,
                           transforms=None,
---
+++ DeepLearning/training/fine_tuning.py
@@ -0,0 +1,268 @@
+import argparse
+import csv
+import os
+from pathlib import Path
+
+import numpy as np
+import tensorflow as tf
+from sklearn.metrics import f1_score
+from tensorflow.keras import optimizers, mixed_precision
+from tensorflow.keras.models import load_model
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras.utils import Progbar
+
+from DeepLearning.config.config import load_config
+from DeepLearning.dataloader.dataloader import get_dataset
+from DeepLearning.losses.losses import focal_loss, ce_loss
+from DeepLearning.models.custom_layers import SpatialConcreteDropout, DropoutAttentionBlock, GroupNormalization
+from DeepLearning.models.unets import build_mc_dropout_unet
+from DeepLearning.processing.transforms import Transforms
+
+# Configure GPU settings
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+mixed_precision.set_global_policy('mixed_float16')
+
+
+def read_last_best_val_f1(log_file_path):
+    try:
+        with open(log_file_path, 'r') as file:
+            lines = file.readlines()
+            if lines:
+                last_line = lines[-1]
+                return float(last_line.split('Validation F1: ')[-1])
+            else:
+                return float('-inf')
+    except FileNotFoundError:
+        return float('-inf')
+
+
+def get_last_epoch(log_file_path):
+    try:
+        with open(log_file_path, 'r') as file:
+            return len(file.readlines())
+    except FileNotFoundError:
+        return 0
+
+
+def gather_from_replicas(strategy, value):
+    gathered_value = strategy.gather(value, axis=0)
+    return gathered_value
+
+
+def compute_f1_score(y_true, y_pred):
+    y_true = np.squeeze(np.argmax(y_true, axis=-1))
+    y_pred = np.squeeze(np.argmax(y_pred, axis=-1))
+    return f1_score(y_true, y_pred, average='macro')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-c", "--config",
+        type=Path,
+        help=r'Config file (YAML), see example in attention_unet/config/example_config.yaml.',
+        default=Path('configurations/configuration.yaml'))
+    args = parser.parse_args()
+    cfg = load_config(args.config)
+    model_prefix = ''
+
+
+    @tf.function(jit_compile=cfg['USE_XLA'])
+    def compiled_train_step(batch, class_weights):
+        with tf.GradientTape() as tape:
+            x, y = batch
+            probs = model(x, training=True)
+            loss = focal_loss(y_true=y, y_pred=probs, alpha_weights=class_weights)
+            scaled_total_loss = optimizer.get_scaled_loss(loss)
+        scaled_grads = tape.gradient(scaled_total_loss, model.trainable_weights)
+        grads = optimizer.get_unscaled_gradients(scaled_grads)
+        return loss, grads
+
+
+    def train_step(batch=None, class_weights=1):
+        loss, grads = strategy.run(compiled_train_step, args=(batch, class_weights))
+        per_replica_loss = strategy.reduce(tf.distribute.ReduceOp.SUM, loss, axis=None)
+        strategy.run(apply_gradients, args=(grads,))
+        return per_replica_loss
+
+
+    @tf.function
+    def apply_gradients(grads):
+        optimizer.apply_gradients(zip(grads, model.trainable_weights))
+
+
+    @tf.function(jit_compile=cfg['USE_XLA'])
+    def compiled_validation_step(batch):
+        x_val, y_val = batch
+        val_probs = model(x_val, training=False)
+        val_loss = ce_loss(y_true=y_val, y_pred=val_probs)
+        return val_loss, val_probs, y_val
+
+
+    def validation_step(batch=None):
+        val_loss, val_probs, y_val = strategy.run(compiled_validation_step, args=(batch,))
+        per_replica_val_loss = strategy.reduce(tf.distribute.ReduceOp.SUM, val_loss, axis=None)
+        return per_replica_val_loss, val_probs, y_val
+
+
+    checkpoint_dir = f"{cfg['MODEL_DIR']}/{model_prefix}_{cfg['MODEL_NAME']}/"
+
+    if not os.path.exists(checkpoint_dir):
+        os.makedirs(checkpoint_dir)
+
+    with open(checkpoint_dir + 'paras.txt', 'w') as config_log:
+        print(cfg, file=config_log)
+    model_save_log_path = f'{checkpoint_dir}{model_prefix}_model_save_log.txt'
+    best_val_f1 = read_last_best_val_f1(model_save_log_path)
+    error_log_path = f'{checkpoint_dir}{model_prefix}_errors_{cfg["MODEL_NAME"][:-3]}.txt'
+    p_logit_log_path = f'{checkpoint_dir}{model_prefix}_p_logit_{cfg["MODEL_NAME"][:-3]}.csv'
+    start_epoch = get_last_epoch(error_log_path)
+
+    strategy = tf.distribute.MirroredStrategy()
+    with strategy.scope():
+        options = tf.data.Options()
+        options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.AUTO
+        transforms = Transforms()
+        class_weights = tf.constant(cfg['CLASS_WEIGHTS'], dtype=tf.float32)
+
+        global_batch_size = cfg['BATCH_SIZE'] * strategy.num_replicas_in_sync
+        original_learning_rate = cfg['LEARNING_RATE']
+        scaling_factor = global_batch_size / cfg['BATCH_SIZE']
+        adjusted_learning_rate = original_learning_rate * scaling_factor
+
+        global_steps = cfg['STEPS'] // cfg['BATCH_SIZE'] // strategy.num_replicas_in_sync
+        training_ds = get_dataset(
+            cfg=cfg,
+            repeat=True,
+            shuffle=True,
+            transforms=transforms,
+            filelists=cfg['TRAINING_LIST'],
+            batch_size=global_batch_size
+        ).take(global_steps).with_options(options)
+        distributed_training_ds = strategy.experimental_distribute_dataset(training_ds)
+
+        global_val_batch_size = cfg['VAL_BATCH_SIZE'] * strategy.num_replicas_in_sync
+        global_val_steps = cfg['VAL_STEPS'] // cfg['BATCH_SIZE'] // strategy.num_replicas_in_sync
+        validation_ds = get_dataset(
+            cfg=cfg,
+            repeat=False,
+            shuffle=False,
+            transforms=None,
+            filelists=cfg['TESTING_LIST'],
+            batch_size=global_val_batch_size
+        ).take(global_val_steps).with_options(options)
+        distributed_validation_ds = strategy.experimental_distribute_dataset(validation_ds)
+
+        optimizer = optimizers.Adam(learning_rate=adjusted_learning_rate)
+        optimizer = mixed_precision.LossScaleOptimizer(optimizer)
+
+        checkpoint_model = f"{checkpoint_dir}{model_prefix}_ckpt_{cfg['MODEL_NAME']}"
+        best_model = f"{checkpoint_dir}{model_prefix}_best_{cfg['MODEL_NAME']}"
+
+        if os.path.exists(checkpoint_model):
+            tf.print(f"Restoring from model checkpoint {checkpoint_model}")
+            model = load_model(
+                filepath=checkpoint_model,
+                compile=False,
+                custom_objects={
+                    'DropoutAttentionBlock': DropoutAttentionBlock,
+                    'GroupNormalization': GroupNormalization,
+                    'SpatialConcreteDropout': SpatialConcreteDropout,
+                }
+            )
+        else:
+            tf.print('Building new model...')
+            model = build_mc_dropout_unet(
+                input_size=cfg['INPUT_SIZE'],
+                num_classes=cfg['OUT_CHANNELS'],
+                num_levels=cfg['N_MODEL_LEVELS'],
+                num_conv_per_level=cfg['N_CONV_PER_LAYER'],
+                num_filters=cfg['N_FILTERS'],
+                regularisation=l2(),
+                use_attention=cfg['USE_ATTENTION'],
+                activation=tf.keras.layers.LeakyReLU(),
+                return_logits=False
+            )
+        model.compile()
+        model.summary()
+
+    if not os.path.exists(p_logit_log_path):
+        with open(p_logit_log_path, 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            header = ['Epoch'] + [layer.name for layer in model.layers if isinstance(layer, SpatialConcreteDropout)]
+            writer.writerow(header)
+
+    for epoch in range(start_epoch, cfg['EPOCHS']):
+        ds = iter(distributed_training_ds)
+        progbar_metrics = ['Epoch', 'focal_loss' if cfg['USE_FOCAL_LOSS'] else 'ce_loss']
+        train_pb = Progbar(global_steps, stateful_metrics=progbar_metrics)
+        avg_loss = 0.0
+        num_batches = 0
+        epoch_p_logit_values = {layer.name: None for layer in model.layers if isinstance(layer, SpatialConcreteDropout)}
+
+        for _, x, y in ds:
+            loss = train_step((x, y), class_weights)
+            avg_loss += loss
+            num_batches += 1
+
+            for layer in model.layers:
+                if isinstance(layer, SpatialConcreteDropout):
+                    epoch_p_logit_values[layer.name] = layer.p_logit.numpy().mean()
+
+            progbar_values = [
+                ('Epoch', int(epoch) + 1),
+                ('focal_loss' if cfg['USE_FOCAL_LOSS'] else 'ce_loss', avg_loss / num_batches),
+            ]
+
+            train_pb.update(num_batches, values=progbar_values)
+
+        val_ds = iter(distributed_validation_ds)
+        val_pb = Progbar(global_val_steps, stateful_metrics=progbar_metrics)
+        avg_val_loss = 0.0
+        num_val_batches = 0
+        val_f1_scores = []
+        y_true_gathered = []
+        y_probs_gathered = []
+
+        for _, yx, yy in val_ds:
+            val_loss, val_probs, y_true = validation_step((yx, yy))
+            center_x, center_y = cfg['INPUT_SIZE'][0] // 2, cfg['INPUT_SIZE'][1] // 2
+            avg_val_loss += val_loss
+            num_val_batches += 1
+            y_true_gathered.append(gather_from_replicas(strategy, y_true)[:, center_x, center_y, :])
+            y_probs_gathered.append(gather_from_replicas(strategy, val_probs)[:, center_x, center_y, :])
+            val_pb.update(
+                num_val_batches,
+                values=[
+                    ('Epoch', int(epoch) + 1),
+                    ('val_loss', avg_val_loss / num_val_batches),
+                ]
+            )
+
+        # Save p_logit values to CSV
+        with open(p_logit_log_path, 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            row = [epoch + 1] + [epoch_p_logit_values[layer.name] for layer in model.layers if
+                                 isinstance(layer, SpatialConcreteDropout)]
+            writer.writerow(row)
+
+        final_f1_score = compute_f1_score(np.array([y[0, :] for y in y_true_gathered]),
+                                          np.array([y[0, :] for y in y_probs_gathered]))
+        with open(f'{checkpoint_dir}{model_prefix}_errors_{cfg["MODEL_NAME"][:-3]}.txt', 'a') as f:
+            f.write(
+                f'{avg_loss.numpy() / num_batches}\t'
+                f'{avg_val_loss.numpy() / num_val_batches}\t'
+                f'{final_f1_score}\n'
+            )
+
+        model.save(checkpoint_model)
+
+        if final_f1_score > best_val_f1:
+            best_val_f1 = final_f1_score
+            model.save(best_model)
+            with open(model_save_log_path, 'a') as save_log:
+                save_log.write(
+                    f'Epoch {epoch + 1}: Model saved with Validation F1: {best_val_f1}\n')
